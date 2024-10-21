183235c0336c61390cbecf7412db4b4471a4dddb
vikas236 <saivias461@gmail.com>
2024-10-22 04:07:21 +0530

    add post and get requests

--- index.js
+++ index.js
@@ -26,17 +26,70 @@ app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
 // PostgreSQL client configuration from environment variables
-// const client = new Client({
-//   connectionString: process.env.POSTGRES_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
+const client = new Client({
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-// client.connect();
+client.connect();
 
 app.get("/", async (req, res) => {
-  res.status(200).json("Welcome, your app is working well");
+  res.status(200).json("Hello");
+});
+
+app.get("/gallery", async (req, res) => {
+  try {
+    // Query to select all data from the table
+    const query = "SELECT * FROM drnaveengallery;";
+    const result = await client.query(query);
+
+    // Send the result rows as JSON
+    res.status(200).json(result.rows);
+  } catch (err) {
+    console.error("Error executing query", err.stack);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.post("/removeimg", async (req, res) => {
+  const { data } = req.body;
+  const imageData = data;
+
+  try {
+    // Query to select all data from the table
+    const query = "DELETE FROM drnaveengallery WHERE image_data = $1;";
+    const result = await client.query(query, [data]);
+
+    // Send the result rows as JSON
+    res.status(200).json(result.rows);
+  } catch (err) {
+    console.error("Error executing query", err.stack);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.post("/send-string", async (req, res) => {
+  const { data } = req.body;
+  const imageData = data;
+
+  if (!imageData) {
+    return res.status(400).json({ error: "No image data provided" });
+  }
+
+  const query = `INSERT INTO drnaveengallery (image_data) VALUES ($1)`;
+
+  try {
+    // Insert image data as a bytea (binary) type
+    await client.query(query, [imageData]);
+    res
+      .status(200)
+      .json({ message: "image uploaded successfully", received: imageData });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: "No image data provided" });
+  }
 });
 
 // Start the server
