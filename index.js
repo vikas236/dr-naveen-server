acd09d0b933deb0ea849111de72db9027754201b
vikas236 <saivias461@gmail.com>
2024-10-22 05:04:09 +0530

    update cors settings

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const port = 3000;
 
 const cors = require("cors");
 app.use(cors());
+app.use((req, res, next) => {
+  // Allow access from every, eliminate CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.removeHeader("x-powered-by");
+  // Set the allowed HTTP methods to be requested
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  // Headers clients can use in their requests
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  // Allow request to continue and be handled by routes
+  next();
+});
 
 // Parse JSON and URL-encoded bodies
 app.use(bodyParser.json({ limit: "10mb" }));
